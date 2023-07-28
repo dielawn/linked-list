1ab0570affe2dd9c65f32030b507d8d9c12fef84
dielawn <dielawnmerkill@gmail.com>
2023-07-27 20:16:00 -0600

    linked list functions complete

--- script.js
+++ script.js
@@ -13,66 +13,172 @@ const linkedList = () => {
     size: size = 0,
 
     // add new node to end of list
-    append: append = (value) => {
-        const lastNode = node(value)
-        if (!head) head = lastNode
-        else {
-            const tail = head
-            while (node.next != null) tail = node.next
+    append(value) {
+        const lastNode = node(value);
+  
+        if (!this.head) {
+          this.head = lastNode;
+        } else {
+          let tail = this.head;
+          while (tail.next !== null) {
+            tail = tail.next;
+          }
+          tail.next = lastNode;
         }
+  
+        this.size++;
+      },
 
-        next = lastNode
-        size++
+    prepend(value) {
+        const firstNode = node(value, this.head)
+        this.head = firstNode
+        this.size++
     },
 
-    prepend: prepend = (value) => {
-        const firstNode = node(value)
-        head = firstNode
-        size++
-    },
-
-    insertAt: insertAt = (value, index) => {
-        if (index > 0 && index > size) return append(value)
-        if (index === 0) return prepend(value)
-
-        const newNode = node(value)
-        let current, previous
-        current = head
-        let count = 0
+    insertAt(value, index) {
+        if (index > 0 && index >= this.size) return this.append(value);
+        if (index === 0) return this.prepend(value);
+  
+        const newNode = node(value);
+        let current = this.head;
+        let previous = null;
+        let count = 0;
+  
         while (count < index) {
-            previous = current
-            count++
-            current = next        
+          previous = current;
+          count++;
+          current = current.next;
         }
+  
+        newNode.next = current;
+        previous.next = newNode;
+        this.size++;
+      },
 
-        newNode = current
-        previous = newNode
-        size++
+    getAt(index) {
+        let current = this.head
+        let count = 0
+
+        while (current) {
+            if (count === index) {
+                return current.value
+            }
+            count++
+            current = current.next
+        }
     },
 
-  printListValue: printListValue = () => {
-    let current = head
-        let dataList = []
-        while(current) {
-            console.log(current.value)
-            dataList.push(`${current.value} -> `)
-            
-            current = node.next
-            if (!current) {
-                dataList.push('null')
+    removeAt(index) {
+        if (index < 0 && index > this.size) return 'Invalid index'
+
+        let current = this.head
+        let previous
+        let count = 0
+
+        if (index === 0) this.head = current.next
+        else {
+            while (count < index) {
+                count++
+                previous = current
+                current = current.next
             }
+
+            previous.next = current.next
         }
-        console.log(dataList)
-  } 
+
+        this.size--
+    },
+
+    removeLast() {
+        let lastIndex = this.size
+        lastIndex--
+        this.removeAt(lastIndex)
+    },
+
+    isListed(value) {
+        let current = this.head
+
+        while (current) {
+            if (value === current.value) {
+                console.log(true, `${value} is listed`)
+                return true
+            }
+
+            current = current.next
+        }
+
+        console.log(false, `${value} not found`)
+        return false        
+    },
+
+    getIndex(value) {
+        let current = this.head
+        let index = 0
+
+        while (current) {
+            if (value === current.value) {
+                console.log( `${value} found at index: ${index}`)
+                return index
+            }
+
+            current = current.next
+            index++
+        }
+        console.log(`${value} not found`)
+        return null
+    },
+
+    remove(value) {
+        let valueIndex = this.getIndex(value)
+        this.removeAt(valueIndex)
+    },
+
+    clearList() {
+        this.head = null
+        this.size = 0
+    },
+
+    printListValue() {
+        let current = this.head;
+        let dataList = [];
+  
+        while (current) {
+          console.log(current.value);
+          dataList.push(`${current.value} -> `);
+  
+          current = current.next;
+          if (!current) {
+            dataList.push('null');
+          }
+        }
+  
+        console.log(dataList.join(''));
+      },
 
     }
 }
 
 const myList = linkedList()
-myList.prepend(100)
-myList.append(200)
+const fullList = [
+myList.append(100),
+myList.append(200),
+myList.append(300),
+myList.append(400),
+myList.append(500),
+myList.append(600),
+myList.append(800),
+myList.prepend(50),
+]
 
+myList.insertAt(150, 2)
+// myList.removeAt(2)
+console.log('getting index 2', myList.getAt(2))
+// myList.removeLast()
+myList.isListed(200)
+myList.getIndex(600)
+myList.remove(500)
 myList.printListValue()
+
 
 
 
